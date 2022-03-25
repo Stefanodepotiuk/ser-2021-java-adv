02f20f8bc566670657ffa5dbc9baa38beaab924c
Stefanodepotiuk <stepan.potiuk@gmail.com>
2022-03-25 12:33:55 +0200

    Initial commit

---
+++ src/main/java/ua/com/owu/ser2021javaadv/Ser2021JavaAdvApplication.java
@@ -0,0 +1,13 @@
+package ua.com.owu.ser2021javaadv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ser2021JavaAdvApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ser2021JavaAdvApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ua/com/owu/ser2021javaadv/Ser2021JavaAdvApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.com.owu.ser2021javaadv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ser2021JavaAdvApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
